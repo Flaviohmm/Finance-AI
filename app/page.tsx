c9be66102d439d7897f3b4558d0db2d94b584084
Flavio Macedo <fhenrique609@gmail.com>
2025-01-28 13:09:51 -0300

    redirect login to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "./_components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Home = () => {
+const Home = async () => {
+  const authResponse = await auth(); // Obter a resposta de autenticação
+
+  console.log(authResponse); // Verifique a estrutura do que foi retornado
+
+  const userID = authResponse.userId;  // Pegue o ID do usuário
+
+  if (!userID) {
+    redirect("/login");
+  }
+
   return (
-    <Button>Hello World</Button>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
   );
 };
 
