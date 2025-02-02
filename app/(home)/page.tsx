ef58d1717b58c708119a14eaeff8bc55f73cf1b7
Flavio Macedo <fhenrique609@gmail.com>
2025-02-02 17:11:55 -0300

    Add Summary Cards

---
+++ app/(home)/page.tsx
@@ -0,0 +1,43 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Navbar from "../_components/navbar";
+import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time-select";
+import { isMatch } from "date-fns";
+
+interface HomeProps {
+  searchParams: {
+    month?: string;
+  };
+}
+
+const Home = async ({ searchParams: { month } }: HomeProps) => {
+  const authResponse = await auth(); // Obter a resposta de autenticação
+
+  const userID = authResponse.userId; // Pegue o ID do usuário
+
+  if (!userID) {
+    redirect("/login");
+  }
+
+  const monthIsInvalid = !month || !isMatch(month, "MM");
+
+  if (monthIsInvalid) {
+    redirect("/?month=01");
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+        <SummaryCards month={month}/>
+      </div>
+    </>
+  );
+};
+
+export default Home;
