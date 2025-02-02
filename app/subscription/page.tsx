a1a66699272e5c6af977f35bcd3a7b129860e350
Flavio Macedo <fhenrique609@gmail.com>
2025-02-02 12:31:04 -0300

    Ensure transactions and subscriptions pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,16 @@
+import { auth } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar"
+import { redirect } from "next/navigation";
 
-const SubscriptionPage = () => {
+const SubscriptionPage = async () => {
+    const authResponse = await auth();
+
+    const userID = authResponse.userId;
+
+    if (!userID) {
+        redirect("/login");
+    }
+    
     return <Navbar />;
 }
 
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,10 +3,22 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-    // Acessar as transações do meu banco de dados
-    const transactions = await db.transaction.findMany({});
+    const authResponse = await auth();
+    const userID = authResponse.userId;
+
+    if (!userID) {
+        redirect("/login");
+    }
+
+    const transactions = await db.transaction.findMany({
+        where: {
+            userId: userID,
+        },
+    });
     
     return (
         <>
